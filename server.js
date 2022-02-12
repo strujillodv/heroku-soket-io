9657a5bc20854bfb46e48efcb0c15f04a0601b8a
Sergio Antonio Trujillo del Valle <strujillodv@gmail.com>
2022-02-12 09:01:24 -0500

    Se agrega CORS y se crea el evento para manejo de datos

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
+
+const path = require('path');
 const express = require('express');
+var cors = require('cors');
+const app = express();
+
 const socketIO = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+// configuración del puerto del servidor
+const port = process.env.PORT || 3001;
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// Ruta a la carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
 
-const io = socketIO(server);
+// Usamos CORS
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
+// Inicio del servidor
+const server =  app.listen(port, () => {
+    console.log(`Server on port ${port}`)
+})
+
+// Inicializamos sokets io con la configuracion del server 
+const io  = socketIO(server,
+  {  cors: {    origin: '*',   methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']  }
+});
+
 
 io.on('connection', (socket) => {
+
   console.log('Client connected');
+
   socket.on('disconnect', () => console.log('Client disconnected'));
+
+  // Escucha los mensajes enviados por el canal de la aplicación
+  socket.on('data:resource', (data) => {
+    // Envia la data a los clientes conectados
+    io.emit('data:resource', data);
+    console.log("Data enviada a los clientes");
+  })
+  
 });
+
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
